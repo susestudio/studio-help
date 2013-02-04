0e25394e3523168abf6f1929d38b0cacc661acb3
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2013-02-04 12:21:34 +0100

    clean before creating the snapshot
    Otherwise we would have unwanted files in the snapshot

--- make_snapshot.sh
+++ make_snapshot.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 [ ! -d build ] || rm -rf build
+git clean -x -f
 VERSION=$(cat VERSION)
 sed -e "s/#VERSION#/$VERSION/g" studio-help.spec.in > studio-help.spec
 mkdir -p build/studio-help-$VERSION
