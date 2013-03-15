4b1f987a0b7b1dc44addcbe693b33b78dc7cc06c
Jordi Massaguer Pla <jmassaguerpla@suse.de>
2013-03-15 12:08:25 +0100

    remove VERSION from master
    Builds from master will contain a timestamp. This will make it easier
    to build it on a continuous way, that is, every commit will create
    a new package. If we were using a VERSION file instead of a timestamp,
    we will end up with lots of versions and lots of tags, what will have
    made git very messy. Moreover, we would have to maintain the VERSION
    file and that could easilly create conflicts in the continuous build
    system and in git tags.

--- make_snapshot.sh
+++ make_snapshot.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env sh
 [ ! -d build ] || rm -rf build
-VERSION=$(cat VERSION)
+if [ -f VERSION ];then
+  VERSION=$(cat VERSION)
+else
+  VERSION=date +%Y%m%d.%H%M%S.0
+fi
 sed -e "s/#VERSION#/$VERSION/g" studio-help.spec.in > studio-help.spec
 mkdir -p build/studio-help-$VERSION
 rsync -av . build/studio-help-$VERSION --exclude build --exclude .git
 ( cd build/studio-help-$VERSION; bundle package )
 ( cd build/ ; tar jcvf ../studio-help-$VERSION.tar.bz2 studio-help-$VERSION )
-git tag v$VERSION
-git push --tags
-
-
+if [ -f VERSION ];then
+  git tag v$VERSION
+  git push --tags
+fi
 
